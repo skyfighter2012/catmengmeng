02a09c9f1b9fdbc29334bbab6a96dc72433cde18
Qinglu-Kooboo <qinglu@kooboo.cn>
2016-01-13 18:13:04 +0800

    dddd

--- Presentation/CatMM.Web/Scripts/_references.js
+++ Presentation/CatMM.Web/Scripts/_references.js
@@ -3,3 +3,4 @@
 /// <reference path="../assets/plugins/bootstrap/js/bootstrap.js" />
 /// <reference path="../assets/plugins/bootstrap/js/npm.js" />
 /// <reference path="../assets/plugins/jquery-1.12.0.min.js" />
+/// <reference path="../assets/scripts/utility.js" />
