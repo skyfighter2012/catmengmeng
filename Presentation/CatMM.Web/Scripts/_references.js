43efd22000d484a92d47b2a276e232e88d91cf44
Qinglu-Kooboo <qinglu@kooboo.cn>
2016-01-13 13:48:48 +0800

    fix

--- Presentation/CatMM.Web/Scripts/_references.js
+++ Presentation/CatMM.Web/Scripts/_references.js
@@ -1,2 +1,4 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="utility.js" />
+/// <reference path="../assets/plugins/bootstrap/js/bootstrap.js" />
+/// <reference path="../assets/plugins/bootstrap/js/npm.js" />
