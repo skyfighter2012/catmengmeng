3598dd95fff30ef96ad9871cbbd3801925bdf0df
qinglu@kooboo.cn <qinglu@kooboo.cn>
2016-05-11 18:13:27 +0800

    fff

--- Globals/CatMM.Shared/SiteConfig.cs
+++ Globals/CatMM.Shared/SiteConfig.cs
@@ -10,11 +10,25 @@ namespace CatMM.Shared
     public static class SiteConfig
     {
         private static string _domain = ConfigurationManager.AppSettings["Domain"];
+
         public static string Domain
         {
             get
             {
                 return _domain;
+            }
+        }
+
+        private static string _googleApiKey = ConfigurationManager.AppSettings["GoogleApiKey"];
+
+        /// <summary>
+        /// Google api key
+        /// </summary>
+        public static string GoogleApiKey
+        {
+            get
+            {
+                return _googleApiKey;
             }
         }
     }
---
+++ Presentation/CatMM.Web/Controllers/GoogleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatMM.Web.Controllers
+{
+    public class GoogleController : Controller
+    {
+        private static string GoogleApiKey = ConfigurationManager.AppSettings["GoogleApiKey"];
+
+        public ActionResult StaticMap()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
