5cb4dec7a7596a7c6c230935659b90c1c9e2f931
qinglu@kooboo.cn <qinglu@kooboo.cn>
2016-05-12 18:22:04 +0800

    add

---
+++ Presentation/CatMM.Web/Controllers/JsPatternController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatMM.Web.Controllers
+{
+    public class JsPatternController : Controller
+    {
+        // GET: J
+        public ActionResult Interface()
+        {
+            return View();
+        }
+    }
+}
