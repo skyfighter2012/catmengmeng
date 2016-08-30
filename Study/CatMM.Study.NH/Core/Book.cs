a44c3b7c13fe927e022f26c7c57beab728db91e2
qinglu@kooboo.cn <qinglu@kooboo.cn>
2016-08-30 18:22:39 +0800

    nh

---
+++ Study/CatMM.Study.NH/Core/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatMM.Study.NH.Core
+{
+    /// <summary>
+    /// Book
+    /// </summary>
+    public class Book : Product
+    {
+        /// <summary>
+        /// ISBN
+        /// </summary>
+        public virtual string ISBN { get; set; }
+
+        /// <summary>
+        /// Author
+        /// </summary>
+        public virtual string Author { get; set; }
+    }
+}
---
+++ Study/CatMM.Study.NH/Core/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatMM.Study.NH.Core
+{
+    /// <summary>
+    /// Movie
+    /// </summary>
+    public class Movie
+    {
+        /// <summary>
+        /// Director
+        /// </summary>
+        public virtual string Director { get; set; }
+    }
+}
